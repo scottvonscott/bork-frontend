bf05f4cea64f2659f3c21f9a83f11b48ce8be2c3
scottvonscott <skeplin17@gmail.com>
2020-09-22 17:09:48 -0700

    a few more containers

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Game = () => {
-  return (
-    <div>
-      <h1>Game Page</h1>
-    </div>
-  );
-};
+class Game extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      something: 1
+    }
+  }
 
+  render() {
+    return(
+      <div>
+        <h1> WTF am I doing?</h1>
+        <p>{this.state.something}</p>
+      </div>
+    )
+  }
+}
 export default Game;
