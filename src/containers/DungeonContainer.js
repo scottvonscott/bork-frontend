8884c6ff264dfa36e1ceaab22fac5749733c3e0b
scottvonscott <skeplin17@gmail.com>
2020-10-01 15:46:53 -0700

    conditional render on monster death

--- src/containers/DungeonContainer.js
+++ src/containers/DungeonContainer.js
@@ -4,6 +4,7 @@ import EventBox from '../components/EventBox'
 import Card from 'react-bootstrap/Card'
 import { connect} from "react-redux"
 import { fetchDungeon } from "../actions/gameActions"
+import Button from 'react-bootstrap/Button'
 
 
 class DungeonContainer extends React.Component {
@@ -16,16 +17,30 @@ class DungeonContainer extends React.Component {
         if (this.props.dungeonReducer.loading) {
             return <h2>Loading Dungeon</h2>
         }
-            console.log("renderdungeon")
-            let dungeon
-            dungeon = this.props.dungeonReducer.dungeon
-            return (
-            <div>
-                <h1>Dungeon Level {dungeon.attributes.level_number}</h1>
-                <p>{dungeon.attributes.entry_text}</p>
-                <MonsterCard monster={this.props.dungeonReducer.monster} monster_health={this.props.fightReducer.monster.monster_game_health} />
-                <EventBox action_outcome={this.props.fightReducer.action_outcome} />
-            </div>)
+        let dungeon
+        dungeon = this.props.dungeonReducer.dungeon
+        
+        // Conditional rendering here for dungeon exit text, monster death, button to fetch new dungeon IF monster_health === 0
+            if (this.props.fightReducer.monster.monster_game_health === 0) {
+                const nextDungeon = dungeon.attributes.level_number + 1
+                debugger
+                return(
+                    <div>
+                        <h1>Dungeon Level {dungeon.attributes.level_number}</h1>
+                        <p>{dungeon.attributes.exit_text}</p>
+                        {/* <Button variant="dark" size="lg" onClick={() => roshambo("1", props)}>Sword</Button> */}
+
+
+                    </div>
+                )
+            }   
+                return (
+                    <div>
+                        <h1>Dungeon Level {dungeon.attributes.level_number}</h1>
+                        <p>{dungeon.attributes.entry_text}</p>
+                        <MonsterCard monster={this.props.dungeonReducer.monster} monster_health={this.props.fightReducer.monster.monster_game_health} />
+                        <EventBox action_outcome={this.props.fightReducer.action_outcome} />
+                    </div>)
     }
 
 
