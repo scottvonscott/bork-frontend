36ea04fd70861ae03ed84f4970131e708eae09ff
scottvonscott <skeplin17@gmail.com>
2020-10-06 14:24:34 -0700

    Built likescore component for highscore page

--- src/components/HighScoreCard.js
+++ src/components/HighScoreCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import LikedScores from './LikedScores'
+
 
 const HighScoreCard = (props) => {
     return(
       <div>
         <h1>High Scores</h1>
-        {props.highScores.data.map(game => <ul>Player: {game.attributes.player_name} - {game.attributes.score}</ul>)}
+        <p>
+        {props.highScores.data.map(game => <LikedScores game={game}/>)
+        }
+        
+        </p>
           </div>
       )
     }
---
+++ src/components/LikedScores.js
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+class LikedScores extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            likes: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState(prevState => (
+            {likes: prevState.likes + 1}
+        ))
+    }
+
+    render(){
+        return(
+
+            <div>
+                <ul>Player Name: {this.props.game.attributes.player_name} | Score: {this.props.game.attributes.score}
+                </ul>
+                <p>Likes: {this.state.likes} <Button variant="secondary" size="small" onClick={this.handleClick}>Like</Button></p>
+            </div>
+        )
+    }
+}
+
+export default LikedScores
