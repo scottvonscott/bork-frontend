5a111a0dd1e9f296eabfd41f09648b5dde9ce1be
scottvonscott <skeplin17@gmail.com>
2020-12-18 19:19:45 -0800

    LootOption useState hook for hover text 1

--- src/components/LootOption.js
+++ src/components/LootOption.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button'
 import { connect } from "react-redux"
+
 import {armor, weapon} from "../actions/lootActions"
 
 const LootOption = (props) => {
     debugger
-    let armorOption = props.loot[1].name
-    let weaponOption = props.loot[0].name
+    const [isShown, showArmorDescription] = useState(false)
+    let armorOption = props.loot[1]
+    let weaponOption = props.loot[0]
     debugger
 
     return(
         <div>
-            <Button variant="danger" size="lg" >{armorOption}</Button>
-            <Button variant="danger" size="lg" >{weaponOption}</Button>
+            <Button variant="danger" size="lg" onMouseEnter={() => showArmorDescription(true)} onMouseLeave={() => showArmorDescription(false)} >{armorOption.name}</Button>
+            <Button variant="danger" size="lg" >{weaponOption.name}</Button>
+
+            {isShown && (
+                <div>
+                    {armorOption.description}
+                    </div>
+            )}
 
         </div>
     )
